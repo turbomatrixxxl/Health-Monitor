61b65d2684672f72084e1758b91c7c6215e8bc9e
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-09-03 10:52:32 +0300

    changed Browser Router with Hash Router  and eliminated 404.html from Public

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import "./styles/variables.css";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+// import { BrowserRouter } from "react-router-dom";
+
+// // Use the correct basename based on the environment
+// const basename =
+//   process.env.NODE_ENV === "production" ? "/Health-Monitor" : "/";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter
+//         future={{
+//           v7_startTransition: true,
+//           v7_relativeSplatPath: true,
+//         }}
+//         basename={basename} // Dynamically set the basename
+//       >
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -6,25 +39,15 @@ import reportWebVitals from "./reportWebVitals";
 import "./styles/variables.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
-
-// Use the correct basename based on the environment
-const basename =
-  process.env.NODE_ENV === "production" ? "/Health-Monitor" : "/";
+import { HashRouter } from "react-router-dom"; // 🔄 schimbat din BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true,
-        }}
-        basename={basename} // Dynamically set the basename
-      >
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
